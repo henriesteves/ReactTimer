7938bfd647564b2dcf6ef39df09b03e01416b413
Henrique Esteves <henriesteves@gmail.com>
2017-04-15 20:24:49 -0300

    Createed timer component

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -1,28 +1,28 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var expect = require('expect');
-var $ = require('jquery');
+var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var Countdown = require('Countdown');
 
 describe('Countdown', () => {
-  it('should exist', () => {
-    expect(Countdown).toExist();
-  });
+    it('should exist', () => {
+        expect(Countdown).toExist();
+    });
 
-  describe('handleSetCountdown', () => {
-
-     it('should set state to started and countdown', (done) => {
+    describe('handleSetCountdown', () => {
+     it('should set state to started and coutdown', (done) => {
         var countdown = TestUtils.renderIntoDocument(<Countdown />);
         countdown.handleSetCountdown(10);
+
         expect(countdown.state.count).toBe(10);
         expect(countdown.state.countdownStatus).toBe('started');
 
         setTimeout(() => {
             expect(countdown.state.count).toBe(9);
             done();
-        }, 1001);
+       }, 1001)
      });
 
      it('should never set count less than zero', (done) => {
@@ -32,10 +32,10 @@ describe('Countdown', () => {
         setTimeout(() => {
             expect(countdown.state.count).toBe(0);
             done();
-        }, 3001);
+        }, 3001)
      });
 
-     it('should pause countdown on paused status', () => {
+     it('should pause countdown on paused status', (done) => {
         var countdown = TestUtils.renderIntoDocument(<Countdown />);
         countdown.handleSetCountdown(3);
         countdown.handleStatusChange('paused');
@@ -47,16 +47,16 @@ describe('Countdown', () => {
         }, 1001);
      });
 
-     it('should reset count on stopped', () => {
+     it('should reset count on stopped', (done) => {
         var countdown = TestUtils.renderIntoDocument(<Countdown />);
         countdown.handleSetCountdown(3);
         countdown.handleStatusChange('stopped');
 
         setTimeout(() => {
-            expect(countdown.state.count).toBe(3);
+            expect(countdown.state.count).toBe(0);
             expect(countdown.state.countdownStatus).toBe('stopped');
             done();
         }, 1001);
      });
-  });
+    });
 });
